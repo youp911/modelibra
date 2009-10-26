6b387a70d46fb4ed8406814f8b97811217200ba5
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-10-26 21:36:14 +0000

    iEducNews-01

---
+++ tags/iEduc/iEducNews-01/src/org/ieducnews/WebLink.java
@@ -0,0 +1,25 @@
+package org.ieducnews;
+
+public class WebLink {
+
+	private String name;
+
+	private String link;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+}
