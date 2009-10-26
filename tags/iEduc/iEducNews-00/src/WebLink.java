f85126c50ff1dc2583d3fa07f7553850187e639e
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-10-26 21:29:38 +0000

    iEducNews-00

---
+++ tags/iEduc/iEducNews-00/src/WebLink.java
@@ -0,0 +1,8 @@
+public class WebLink {
+
+	public static void main(String[] args) {
+		System.out.println("http://news.ycombinator.com/");
+		System.out.println("http://www.techcrunch.com/");
+	}
+
+}
