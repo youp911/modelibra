90e73783cdd6740499158b55d64329a01e6140a2
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-02 13:20:20 +0000

    simplified config path location

--- trunk/iEduc/iEducNews/src/org/ieducnews/server/Start.java
+++ trunk/iEduc/iEducNews/src/org/ieducnews/server/Start.java
@@ -9,16 +9,13 @@ public class Start {
 
 	public static final String CONFIG_FILE_NAME = "jetty-config.xml";
 
-	private String locateJettyConfigPath() {
-		PathLocator pathLocator = new PathLocator();
-		return pathLocator.getClassBasedPath(Start.class, CONFIG_FILE_NAME);
-	}
-
 	public static void main(String[] args) {
-		Start start = new Start();
 		Server jettyServer = null;
+		PathLocator pathLocator = new PathLocator();
 		try {
-			URL jettyConfig = new URL("file:" + start.locateJettyConfigPath());
+			URL jettyConfig = new URL("file:"
+					+ pathLocator.getClassBasedPath(Start.class,
+							CONFIG_FILE_NAME));
 			if (jettyConfig != null) {
 				jettyServer = new Server(jettyConfig);
 				jettyServer.start();
--- trunk/iEduc/iEducNews/src/org/ieducnews/util/PathLocator.java
+++ trunk/iEduc/iEducNews/src/org/ieducnews/util/PathLocator.java
@@ -1,18 +1,3 @@
-/*
- * Modelibra
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package org.ieducnews.util;
 
 import java.io.UnsupportedEncodingException;
@@ -21,31 +6,11 @@ import java.net.URLDecoder;
 
 public class PathLocator {
 
-	public URL getClassBasedUrl(Class<?> claz, String localPath) {
-		return claz.getResource(localPath);
-	}
-
-	/**
-	 * Gets the file path given a local path anchored to the class base.
-	 * 
-	 * @param claz
-	 *            class that is an anchor point
-	 * @param localPath
-	 *            local path
-	 * @return file path
-	 */
 	public String getClassBasedPath(Class<?> claz, String localPath) {
-		URL fileUrl = getClassBasedUrl(claz, localPath);
+		URL fileUrl = claz.getResource(localPath);
 		return getPath(fileUrl);
 	}
 
-	/**
-	 * Gets the file path given a url.
-	 * 
-	 * @param url
-	 *            url
-	 * @return file path
-	 */
 	public String getPath(URL url) {
 		String filePath = null;
 		try {
@@ -53,7 +18,7 @@ public class PathLocator {
 				filePath = URLDecoder.decode(url.getFile(), "UTF-8");
 			}
 		} catch (UnsupportedEncodingException e) {
-			System.out.println(e.getMessage() + " // problem with UTF-8");
+			System.out.println(e.getMessage());
 		}
 		return filePath;
 	}
