55d01305e2191b7f552619aaa7ee564afb41a977
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-02 13:13:53 +0000

    with PathLocator; without PropertiesLoader

--- trunk/iEduc/iEducNews/src/org/ieducnews/server/Start.java
+++ trunk/iEduc/iEducNews/src/org/ieducnews/server/Start.java
@@ -2,20 +2,23 @@ package org.ieducnews.server;
 
 import java.net.URL;
 
+import org.ieducnews.util.PathLocator;
 import org.mortbay.jetty.Server;
 
 public class Start {
 
-	private String findJettyConfigPath() {
-		ConfigProperties configProperties = new ConfigProperties(Start.class);
-		return configProperties.getJettyConfigPath();
+	public static final String CONFIG_FILE_NAME = "jetty-config.xml";
+
+	private String locateJettyConfigPath() {
+		PathLocator pathLocator = new PathLocator();
+		return pathLocator.getClassBasedPath(Start.class, CONFIG_FILE_NAME);
 	}
 
 	public static void main(String[] args) {
 		Start start = new Start();
 		Server jettyServer = null;
 		try {
-			URL jettyConfig = new URL("file:" + start.findJettyConfigPath());
+			URL jettyConfig = new URL("file:" + start.locateJettyConfigPath());
 			if (jettyConfig != null) {
 				jettyServer = new Server(jettyConfig);
 				jettyServer.start();
