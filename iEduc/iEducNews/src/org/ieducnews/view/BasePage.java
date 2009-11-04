39736a6920b9f360edb9acd108126f9f71133bfc
Pascal.Daneault <Pascal.Daneault@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-04 21:38:39 +0000

    Markup inheritance

---
+++ iEduc/iEducNews/src/org/ieducnews/view/BasePage.java
@@ -0,0 +1,20 @@
+package org.ieducnews.view;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.ieducnews.model.WebLink;
+import org.ieducnews.model.WebLinks;
+
+@SuppressWarnings("all")
+public class BasePage extends WebPage {
+
+	public BasePage() {
+	}
+
+}
+
