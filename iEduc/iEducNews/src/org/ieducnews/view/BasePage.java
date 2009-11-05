b46cfd403d339e2eeee573296a82fe5813d0b247
Pascal.Daneault <Pascal.Daneault@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-05 05:26:33 +0000

    MenuPanel

--- iEduc/iEducNews/src/org/ieducnews/view/BasePage.java
+++ iEduc/iEducNews/src/org/ieducnews/view/BasePage.java
@@ -5,15 +5,16 @@ import java.util.List;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.ieducnews.model.WebLink;
-import org.ieducnews.model.WebLinks;
 
 @SuppressWarnings("all")
 public class BasePage extends WebPage {
 
 	public BasePage() {
+		add(new MenuPanel("menuPanel"));
 	}
 
 }
