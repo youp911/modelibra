506fcd9840e013a39bd3450049dfea3d1e335d44
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-07 00:01:10 +0000

    iEducNews-06

---
+++ tags/iEduc/iEducNews-06/src/org/ieducnews/view/HomePage.java
@@ -0,0 +1,36 @@
+package org.ieducnews.view;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.ieducnews.model.WebLink;
+import org.ieducnews.model.WebLinks;
+
+@SuppressWarnings("all")
+public class HomePage extends BasePage {
+
+	public HomePage() {
+		WebApp webApp = (WebApp) getApplication();
+		WebLinks webLinks = webApp.getDomainModel().getWebLinks();
+		WebLinks orderedWebLinks = webLinks.orderByName();
+		add(new WebLinksListView("webLinksList", orderedWebLinks.getList()));
+	}
+
+	private class WebLinksListView extends ListView {
+
+		private WebLinksListView(String wicketId, List webLinks) {
+			super(wicketId, webLinks);
+		}
+
+		protected void populateItem(ListItem listItem) {
+			WebLink webLink = (WebLink) listItem.getModelObject();
+			listItem.add(new ExternalLink("linkUrl", webLink.getLink(), webLink
+					.getName()));
+			listItem.add(new Label("linkLabel", webLink.getLink()));
+		}
+	}
+
+}
