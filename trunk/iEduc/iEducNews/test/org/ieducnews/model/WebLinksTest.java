0045603322c64e95371a5b72f7314f7851c89f5a
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-07 00:27:29 +0000

    tests

--- trunk/iEduc/iEducNews/test/org/ieducnews/model/WebLinksTest.java
+++ trunk/iEduc/iEducNews/test/org/ieducnews/model/WebLinksTest.java
@@ -39,11 +39,56 @@ public class WebLinksTest {
 		WebLinks webLinks = domainModel.getWebLinks();
 		WebLinks orderedWebLinks = webLinks.orderByName();
 
-		Assert.assertFalse(orderedWebLinks.isEmpty());
+		if (!webLinks.isEmpty()) {
+			Assert.assertFalse(orderedWebLinks.isEmpty());
+		}
 		Assert.assertEquals(webLinks.size(), orderedWebLinks.size());
 		Assert.assertNotSame(webLinks, orderedWebLinks);
 
 		orderedWebLinks.output("---Web links ordered by name---");
+	}
+
+	@Test
+	public void webLinksOrderedByDescendingName() throws Exception {
+		WebLinks webLinks = domainModel.getWebLinks();
+		WebLinks orderedWebLinks = webLinks.orderByName(false);
+
+		if (!webLinks.isEmpty()) {
+			Assert.assertFalse(orderedWebLinks.isEmpty());
+		}
+		Assert.assertEquals(webLinks.size(), orderedWebLinks.size());
+		Assert.assertNotSame(webLinks, orderedWebLinks);
+
+		orderedWebLinks.output("---Web links ordered by descending name---");
+	}
+
+	@Test
+	public void webLinkNotRemoved() throws Exception {
+		WebLinks webLinks = domainModel.getWebLinks();
+		WebLink calendar = new WebLink();
+		calendar.setName("Class Calendar");
+		calendar
+				.setLink("http://java.sun.com/javase/6/docs/api/java/util/Calendar.html");
+
+		WebLink gregorianCalendar = new WebLink();
+		gregorianCalendar.setName("Class GregorianCalendar");
+		gregorianCalendar
+				.setLink("http://java.sun.com/javase/6/docs/api/java/util/GregorianCalendar.html");
+
+		webLinks.add(calendar);
+		Assert.assertFalse(webLinks.remove(gregorianCalendar));
+	}
+
+	@Test
+	public void webLinkRemoved() throws Exception {
+		WebLinks webLinks = domainModel.getWebLinks();
+		WebLink gregorianCalendar = new WebLink();
+		gregorianCalendar.setName("Class GregorianCalendar");
+		gregorianCalendar
+				.setLink("http://java.sun.com/javase/6/docs/api/java/util/GregorianCalendar.html");
+
+		webLinks.add(gregorianCalendar);
+		Assert.assertTrue(webLinks.remove(gregorianCalendar));
 	}
 
 }
