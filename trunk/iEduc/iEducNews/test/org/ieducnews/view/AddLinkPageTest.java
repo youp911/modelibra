c85f9bcf897594d993b3f81483980d3c4dc7fe86
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-08 03:32:25 +0000

    form tests

--- trunk/iEduc/iEducNews/test/org/ieducnews/view/AddLinkPageTest.java
+++ trunk/iEduc/iEducNews/test/org/ieducnews/view/AddLinkPageTest.java
@@ -1,6 +1,8 @@
 package org.ieducnews.view;
 
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.util.tester.WicketTester;
@@ -21,6 +23,9 @@ public class AddLinkPageTest {
 	public void containComponents() {
 		tester.assertComponent("menu", Panel.class);
 		tester.assertComponent("form", Form.class);
+		tester.assertComponent("form:name", RequiredTextField.class);
+		tester.assertComponent("form:link", RequiredTextField.class);
+		tester.assertComponent("form:save", Button.class);
 		tester.assertComponent("feedback", FeedbackPanel.class);
 		tester.assertComponent("footer", Panel.class);
 	}
