99d61b33302a15b3c54b54b5128be1c45cff2360
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-08 03:20:17 +0000

    some Wicket testing

--- iEduc/iEducNews/test/org/ieducnews/view/AddLinkPageTest.java
+++ iEduc/iEducNews/test/org/ieducnews/view/AddLinkPageTest.java
@@ -1,27 +1,27 @@
 package org.ieducnews.view;
 
-import org.apache.wicket.markup.html.list.PageableListView;
-import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class HomePageTest {
+public class AddLinkPageTest {
 
 	private static WicketTester tester;
 
 	@BeforeClass
 	public static void prepareTester() {
 		tester = new WicketTester(new WebApp());
-		tester.startPage(HomePage.class);
+		tester.startPage(AddLinkPage.class);
 	}
 
 	@Test
 	public void containComponents() {
 		tester.assertComponent("menu", Panel.class);
-		tester.assertComponent("webLinks", PageableListView.class);
-		tester.assertComponent("navigator", PagingNavigator.class);
+		tester.assertComponent("form", Form.class);
+		tester.assertComponent("feedback", FeedbackPanel.class);
 		tester.assertComponent("footer", Panel.class);
 	}
 
