bd17d4214c4fd76be671cf5d753df19c9e380b9e
Pascal.Daneault <Pascal.Daneault@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-08 20:39:37 +0000

    - HomeLink - A few tests (not finished)

--- trunk/iEduc/iEducNews/src/org/ieducnews/view/navigation/FooterPanel.java
+++ trunk/iEduc/iEducNews/src/org/ieducnews/view/navigation/FooterPanel.java
@@ -3,7 +3,7 @@ package org.ieducnews.view.navigation;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.ieducnews.view.generic.NewLink;
+import org.ieducnews.view.generic.HomeLink;
 import org.ieducnews.view.staticpages.AboutPage;
 import org.ieducnews.view.weblink.AddLinkPage;
 
@@ -13,7 +13,7 @@ public class FooterPanel extends Panel {
 
 	public FooterPanel(String id) {
 		super(id);
-		add(new NewLink("new"));
+		add(new HomeLink("new"));
 		add(new BookmarkablePageLink<WebPage>("about", AboutPage.class));
 		add(new BookmarkablePageLink<WebPage>("submit", AddLinkPage.class));
 	}
