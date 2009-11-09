20816506729d912eae64aacd3246e273c8378848
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-09 02:46:48 +0000

    iEducNews-08

---
+++ iEducNews-08/test/org/ieducnews/view/HomePageTest.java
@@ -0,0 +1,72 @@
+package org.ieducnews.view;
+
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.util.tester.WicketTester;
+import org.ieducnews.model.DomainModel;
+import org.ieducnews.model.config.ModelProperties;
+import org.ieducnews.view.weblink.AddLinkPage;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HomePageTest {
+
+	private static DomainModel domainModel;
+
+	private static WicketTester tester;
+
+	@BeforeClass
+	public static void beforeTests() {
+		ModelProperties modelProperties = new ModelProperties(
+				AboutPageTest.class);
+		domainModel = new DomainModel(modelProperties);
+		domainModel = domainModel.load();
+
+		WebApp webApp = new WebApp();
+		webApp.setDomainModel(domainModel);
+
+		tester = new WicketTester(webApp);
+		tester.startPage(HomePage.class);
+	}
+
+	@Test
+	public void containComponents() {
+		tester.assertComponent("menu", Panel.class);
+		tester.assertComponent("menu:new", Link.class);
+		tester.assertComponent("menu:submit", BookmarkablePageLink.class);
+		tester.assertComponent("menu:about", BookmarkablePageLink.class);
+		tester.assertComponent("webLinks", PageableListView.class);
+		if (domainModel.getWebLinks().size() <= HomePage.NUMBER_OF_LINKS_ON_ONE_PAGE) {
+			tester.assertInvisible("navigator");
+		} else {
+			tester.assertComponent("navigator", PagingNavigator.class);
+			tester.assertVisible("navigator");
+		}
+		tester.assertComponent("footer", Panel.class);
+		tester.assertComponent("footer:new", Link.class);
+		tester.assertComponent("footer:submit", BookmarkablePageLink.class);
+		tester.assertComponent("footer:about", BookmarkablePageLink.class);
+	}
+
+	@Test
+	public void navigateToHome() {
+		tester.clickLink("menu:new");
+		tester.assertRenderedPage(HomePage.class);
+	}
+
+	@Test
+	public void navigateToSubmitLink() {
+		tester.clickLink("menu:submit");
+		tester.assertRenderedPage(AddLinkPage.class);
+	}
+
+	@Test
+	public void navigateToAbout() {
+		tester.clickLink("menu:about");
+		tester.assertRenderedPage(AboutPage.class);
+	}
+
+}
