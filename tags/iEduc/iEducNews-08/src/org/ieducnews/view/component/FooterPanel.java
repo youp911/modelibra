7b549a40ca796f1b31b274bdd64580d8f4777a00
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-09 02:46:48 +0000

    iEducNews-08

---
+++ tags/iEduc/iEducNews-08/src/org/ieducnews/view/component/FooterPanel.java
@@ -0,0 +1,20 @@
+package org.ieducnews.view.component;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.ieducnews.view.AboutPage;
+import org.ieducnews.view.weblink.AddLinkPage;
+
+public class FooterPanel extends Panel {
+
+	private static final long serialVersionUID = 1;
+
+	public FooterPanel(String id) {
+		super(id);
+		add(new HomeLink("new"));
+		add(new BookmarkablePageLink<WebPage>("about", AboutPage.class));
+		add(new BookmarkablePageLink<WebPage>("submit", AddLinkPage.class));
+	}
+
+}
---
+++ tags/iEduc/iEducNews-08/src/org/ieducnews/view/weblink/AddLinkPage.java
@@ -0,0 +1,66 @@
+package org.ieducnews.view.weblink;
+
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.validation.validator.UrlValidator;
+import org.ieducnews.model.WebLink;
+import org.ieducnews.model.WebLinks;
+import org.ieducnews.view.BasePage;
+import org.ieducnews.view.HomePage;
+import org.ieducnews.view.WebApp;
+
+public class AddLinkPage extends BasePage {
+
+	public AddLinkPage() {
+		WebApp webApp = (WebApp) getApplication();
+		WebLinks webLinks = webApp.getDomainModel().getWebLinks();
+		WebLink webLink = new WebLink();
+
+		Form<WebLink> form = new Form<WebLink>("form",
+				new CompoundPropertyModel<WebLink>(webLink));
+		RequiredTextField<String> nameField = new RequiredTextField<String>(
+				"name");
+		form.add(nameField);
+		RequiredTextField<String> linkField = new RequiredTextField<String>(
+				"link");
+		linkField.add(new UrlValidator());
+		form.add(linkField);
+		form.add(new SaveButton("save", webApp, webLinks, webLink));
+		add(form);
+		add(new FeedbackPanel("feedback"));
+	}
+
+	private class SaveButton extends Button {
+
+		private static final long serialVersionUID = 1;
+
+		private WebApp webApp;
+
+		private WebLinks webLinks;
+
+		private WebLink webLink;
+
+		private SaveButton(String wicketId, WebApp webApp, WebLinks webLinks,
+				WebLink webLink) {
+			super(wicketId);
+			this.webApp = webApp;
+			this.webLinks = webLinks;
+			this.webLink = webLink;
+		}
+
+		@Override
+		public void onSubmit() {
+			if (webLinks.contains(webLink.getName())) {
+				error("this name exists already.");
+			} else {
+				webLinks.add(webLink);
+				webApp.getDomainModel().save();
+				setResponsePage(new HomePage());
+			}
+		}
+	}
+
+}
