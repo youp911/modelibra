5e3a30c2912eb06183d9f4384ecf464af6b6cf58
Pascal.Daneault <Pascal.Daneault@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-12 06:34:20 +0000

    Create Account & EditMemberPage (not finished)

---
+++ trunk/iEduc/iEducNews/src/org/ieducnews/view/concept/member/EditMemberPage.java
@@ -0,0 +1,12 @@
+package org.ieducnews.view.concept.member;
+
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.ieducnews.view.BasePage;
+
+
+public class EditMemberPage extends BasePage {
+	public EditMemberPage() {
+		
+	}
+	
+}
