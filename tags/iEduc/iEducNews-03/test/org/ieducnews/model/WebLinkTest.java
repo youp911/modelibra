03ae5bd19b273b8155a617f7078e5e91adfef822
dzenanr <dzenanr@c25eb2fc-9753-11de-83f8-39e71e4dc75d>
2009-11-21 17:36:54 +0000

    for iteration in a test

--- tags/iEduc/iEducNews-03/test/org/ieducnews/model/WebLinkTest.java
+++ tags/iEduc/iEducNews-03/test/org/ieducnews/model/WebLinkTest.java
@@ -26,4 +26,24 @@ public class WebLinkTest {
 		Assert.assertTrue(webLinks.contains(webLink02));
 	}
 
+	@Test
+	public void webLinksTraversed() throws Exception {
+		WebLink webLink01 = new WebLink();
+		webLink01.setName("Hacker News");
+		webLink01.setLink("http://news.ycombinator.com/");
+
+		WebLink webLink02 = new WebLink();
+		webLink02.setName("TechCrunch");
+		webLink02.setLink("http://www.techcrunch.com/");
+
+		ArrayList webLinks = new ArrayList();
+		webLinks.add(webLink01);
+		webLinks.add(webLink02);
+
+		for (Object object : webLinks) {
+			WebLink webLink = (WebLink) object;
+			System.out.println("Web link name: " + webLink.getName());
+		}
+	}
+
 }
